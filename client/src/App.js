8637d7a45d619dd0824532d069593680a837a5da
Brian Juma <bjuma0985@gmail.com>
2025-03-25 22:51:47 +0300

    setting backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import CustomNavbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Upload from "./pages/Upload";
